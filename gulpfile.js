7882fe755887b46e1022e4fa3ad30128e2fb5d29
sudogem <sudogem@gmail.com>
2017-08-29 16:49:55 +0800

    Update gulpfile.js, config.json

--- gulpfile.js
+++ gulpfile.js
@@ -5,37 +5,52 @@ var gulpNgConfig = require('gulp-ng-config');
 
 var nodeEnv = process.env.NODE_ENV || 'development';
 console.log('nodeEnv:',nodeEnv);
+var configureSetup;
 
-var configureSetup  = {
-  constants: {
-    common: {
-      version: '1.0',
-      appName: 'Helloworld app'
-    },
-    env: {
-      development: {
-        apiUrl: process.env.MY_NODE_ENV_APIURL_DEV,
+switch(nodeEnv) {
+  case 'development':
+    configureSetup = {
+      constants: {
+        common: {
+          version: '1.0',
+          appName: 'Helloworld app'
+        },
         siteName: 'Helloworld App (DEV)',
+        apiUrl: process.env.MY_NODE_ENV_APIURL_DEV,
         cloudant: {
-          account: process.env.CLOUDANTDB_DEV_ACCOUNT,
-          password: process.env.CLOUDANTDB_DEV_PASSWORD
-        }
-      },
-      production: {
-        apiUrl: process.env.MY_NODE_ENV_APIURL_PROD,
-        siteName: 'Helloworld App (PROD)',
-         cloudant: {
-          account: process.env.CLOUDANTDB_PROD_ACCOUNT,
-          password: process.env.CLOUDANTDB_PROD_PASSWORD
+          account: process.env.CLOUDANTDB_DEV_ACCOUNT || 'cloudant_dev_user',
+          password: process.env.CLOUDANTDB_DEV_PASSWORD || 'cloudant_dev_pass'
+        },
+        mongoDB: {
+          mongoDB_URL: process.env.mongoDB_DEV_URL || 'mongodb://local-dev-mongo/db'
         }
       }
     }
-  }
+    break;
+  case 'production':
+    configureSetup = {
+      constants: {
+        common: {
+          version: '1.0',
+          appName: 'Helloworld app'
+        },
+        siteName: 'Helloworld App (PROD)',
+        apiUrl: process.env.MY_NODE_ENV_APIURL_PROD || 'prod-helloapp.com',
+        cloudant: {
+          account: process.env.CLOUDANTDB_PROD_ACCOUNT || 'cloudant_prod_user',
+          password: process.env.CLOUDANTDB_PROD_PASSWORD || 'cloudant_prod_pass'
+        },
+        mongoDB: {
+          mongoDB_URL: process.env.mongoDB_PROD_URL || 'mongodb://production-mongo/db'
+        }
+      }
+    }
+    break;
 }
 
 var makeConfig = function() {
   return gulp.src('config.json')
-             .pipe(gulpNgConfig('myApp.config', {environment: ['env.'+ nodeEnv, 'common']}))
+             .pipe(gulpNgConfig('myApp.config', configureSetup))
              .pipe(gulp.dest('client'));
 }
 
